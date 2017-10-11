de00e0492d913d39c6bf8506455ff3779e41120f
siskon <xiongyq7@mail2.sysu.edu.cn>
2017-10-11 20:19:47 +0800

    queue

--- Project 2/queue.hpp
+++ Project 2/queue.hpp
@@ -1,20 +1,47 @@
 #ifndef MY_QUEUE
 #define MY_QUEUE
+#include <string>
+#define MAX_QUEUE_SIZE 50
 
 template<typename dataType>
 class queue{
 private:
-    dataType storage[50];
+    dataType storage[MAX_QUEUE_SIZE];
     unsigned head;
     unsigned rear;
+    unsigned size;
     
 public:
-    queue();
-    bool push();
-    bool pop();
-    bool front();
-    bool isEmpty();
-    unsigned size();
+    queue():head(0), rear(0), size(0){}
+    bool push(const dataType& dataIn){
+        if(size != MAX_QUEUE_SIZE){
+            ++size;
+            storage[++head] = dataIn; 
+            return true
+        }
+        return false;
+    }
+    bool pop(){
+        if(size != 0){
+            ++rear;
+            return true;
+        }
+        return false;
+    }
+    dataType& front(){
+        if(size != 0){
+            return storage[rear];
+        }
+        else{
+            throw std::string("Error: Trying to get top from an empty stack.");
+        }
+    }
+    bool isEmpty(){
+        return size == 0;
+    }
+    unsigned size(){
+        return size;
+    }
 };
 
 
