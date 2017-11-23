2bf2d9c6fcd473795639648ff6c122cd26783222
SiskonEmilia <xiongyq7@mail2.sysu.edu.cn>
2017-11-23 23:46:04 +0800

    final version

--- Experiment 5/tree.hpp
+++ Experiment 5/tree.hpp
@@ -4,70 +4,118 @@
 #include <iostream>
 using namespace std;
 
+struct Node {
+	int data;
+	unsigned left;
+	unsigned right;
+	Node(const int data = 0, const unsigned left = 0, const unsigned right = 0) : data(data), left(left), right(right) {}
+};
+
 class BTree {
 public:
-  BTree():_size(0), _presentStorage(10000) {
-    _storage = new int[_presentStorage];
-  }
+	BTree() :_size(0), _presentStorage(10000) {
+		_storage = new Node[_presentStorage];
+		_is = new bool[_presentStorage];
+		for (unsigned i = 0; i < _presentStorage; ++i) {
+			_is[i] = false;
+		}
+	}
 
-  ~BTree() {
-    delete[] _storage;
-  }
-  
-  bool empty() const {
-    return _size == 0;
-  }
+	~BTree() {
+		delete[] _storage;
+		delete[] _is;
+	}
 
-  unsigned size() const {
-    return _size;
-  }
+	bool empty() const {
+		return _size == 0;
+	}
 
-  void append (const int& data) {
-    if (_size == _presentStorage)
-      extendStorage();
-    _storage[_size++] = data;
-  }
+	unsigned size() const {
+		return _size;
+	}
 
-  int& operator[] (const unsigned& index) {
-    if (index > _size)
-      throw "RANGE_ERROR";
-    return _storage[index - 1];
-  }
+	void append(const int& data) {
+		if (_size == _presentStorage)
+			extendStorage();
+		if (_size == 0) {
+			_storage[0].data = data;
+			_is[0] = true;
+		}
+		else
+			for (unsigned i = 0; i < _presentStorage; ++i) {
+				if (!_is[i]) {
+					_storage[i].data = data;
+					if (_storage[(i + 1) / 2 - 1].left != 0) {
+						_storage[(i + 1) / 2 - 1].right = i + 1;
+					}
+					else {
+						_storage[(i + 1) / 2 - 1].left = i + 1;
+					}
+					_is[i] = true;
+					break;
+				}
+			}
+		++_size;
+	}
 
-  void print (unsigned position = 1, unsigned counter = 0) {
-    if (position > _size)
-      return;
-    for (int i = 0; i < counter; ++i)
-      cout << '|';
-    cout << _storage[position - 1] << endl;
-    print(position * 2, counter + 1);
-    print(position * 2 + 1, counter + 1);
-  }
+	int& operator[] (const unsigned& index) {
+		if (index > _presentStorage || !_is[index - 1])
+			throw "RANGE_ERROR";
+		return _storage[index - 1].data;
+	}
 
-  void interChange(unsigned position = 1) {
-    if (position > _size)
-      return;
-    auto temp = _storage[position - 1];
-    _storage[position - 1] = _storage[position];
-    _storage[position] = temp;
-    interChange(position * 2);
-    interChange(position * 2 + 1);
-  }
+	void print(unsigned position = 1, unsigned counter = 0) {
+		if (position > _presentStorage || !_is[position - 1])
+			return;
+		for (unsigned i = 0; i < counter; ++i)
+			cout << "  ";
+		cout << "|--";
+		cout << _storage[position - 1].data;
+
+		cout << endl;
+		if (_storage[position - 1].left != 0) {
+			print(_storage[position - 1].left, counter + 1);
+		}
+		if (_storage[position - 1].right != 0) {
+			print(_storage[position - 1].right, counter + 1);
+		}
+	}
+
+	void interChange(unsigned position = 1) {
+		if (position > _presentStorage || !_is[position - 1])
+			return;
+
+		auto temp = _storage[position - 1].left;
+		_storage[position - 1].left = _storage[position - 1].right;
+		_storage[position - 1].right = temp;
+
+		if (_storage[position - 1].left != 0) {
+			interChange(_storage[position - 1].left);
+		}
+		if (_storage[position - 1].right != 0) {
+			interChange(_storage[position - 1].right);
+		}
+	}
 
 private:
-  void extendStorage() {
-    _presentStorage *= 2;
-    auto temp = new int[_presentStorage];
-    for (unsigned i = 0; i < _size; ++i) {
-      temp[i] = _storage[i];
-    }
-    delete[] _storage;
-    _storage = temp;
-  }
+	void extendStorage() {
+		_presentStorage *= 2;
+		auto temp = new Node[_presentStorage];
+		auto istemp = new bool[_presentStorage];
+		for (unsigned i = 0; i < _presentStorage / 2; ++i) {
+			temp[i] = _storage[i];
+			istemp[i] = _is[i];
+		}
+		delete[] _storage;
+		delete[] _is;
+		_storage = temp;
+		_is = istemp;
+	}
 
-  int *_storage;
-  unsigned _size;
-  unsigned _presentStorage;
+	Node *_storage;
+	bool *_is;
+	unsigned _size;
+	unsigned _presentStorage;
 };
 
 #endif
