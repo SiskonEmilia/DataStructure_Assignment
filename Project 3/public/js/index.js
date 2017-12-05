2a59c31f082d1387e67e4005c33b9f182a4b1b2e
SiskonEmilia <xiongyq7@mail2.sysu.edu.cn>
2017-12-05 18:45:31 +0800

    add server-end

--- Project 3/public/js/index.js
+++ Project 3/public/js/index.js
@@ -1,17 +1,54 @@
 $(function () {
+  const OL_Action_Root = "http://localhost:8888";
+
   $.contextMenu ({
     selector: 'span',
     callback: function (key, options) {
-      var m = "clicked: " + key;
-      window.console && console.log(m) || alert(m);
+      switch(key) {
+        case 'addParent':
+          getInfo();
+          Req_ajax();
+        break;
+        
+        case 'addBrother':
+          getInfo();
+        break;
+        
+        case 'addCouple':
+          getInfo();
+        break;
+        
+        case 'addChild':
+          getInfo();
+        break;
+        
+        case 'edit':
+          getInfo();
+        break;
+        
+        case 'delete':
+        
+        break;
+        
+        case 'detail':
+        
+        break;
+        
+        case 'quit':
+          window.close();
+        break;
+        
+        default:
+        break;
+      }
     },
     items: {
       "Add...": { name: "Add...", icon: "fa-plus",
         items: {
-          "Add to parent": { name: "Parent", icon: "fa-user-plus"},
-          "Add to brother": { name: "Brother", icon: "fa-user-plus"},
-          "Add to couple": { name: "Couple", icon: "fa-user-plus"},
-          "Add to children": { name: "Child", icon: "fa-user-plus"}
+          "addParent": { name: "Parent", icon: "fa-user-plus"},
+          "addBrother": { name: "Brother", icon: "fa-user-plus"},
+          "addCouple": { name: "Couple", icon: "fa-user-plus"},
+          "addChild": { name: "Child", icon: "fa-user-plus"}
         }
       },
       "detail": { name: "Detail", icon: "fa-info-circle"},
@@ -19,43 +56,50 @@ $(function () {
       "delete": { name: "Delete", icon: "delete" },
       "sep1": "---------",
       "quit": {
-        name: "Quit", icon: function () {
-          return 'context-menu-icon context-menu-icon-quit';
-        }
+        name: "Quit", icon: 'context-menu-icon context-menu-icon-quit'
       },
       
-    },
-    trigger: "left"
+    }
   });
 
-  $.contextMenu ({
-    selector: 'span',
-    callback: function (key, options) {
-      var m = "clicked: " + key;
-      window.console && console.log(m) || alert(m);
-    },
-    items: {
-      "Add...": { name: "Add...", icon: "fa-plus",
-        items: {
-          "Add to parent": { name: "Parent", icon: "fa-user-plus"},
-          "Add to brother": { name: "Brother", icon: "fa-user-plus"},
-          "Add to couple": { name: "Couple", icon: "fa-user-plus"},
-          "Add to children": { name: "Child", icon: "fa-user-plus"}
-        }
-      },
-      "detail": { name: "Detail", icon: "fa-info-circle"},
-      "edit": { name: "Edit", icon: "fa-pencil-square"},
-      "delete": { name: "Delete", icon: "delete" },
-      "sep1": "---------",
-      "quit": {
-        name: "Quit", icon: function () {
-          return 'context-menu-icon context-menu-icon-quit';
-        }
-      },
-      
-    },
-    trigger: "right"
-  });
+  /* if success, send an array to the callback, which contains all the data needed */
+  function getInfo(callback) {
+    
+  }
 
+  function sendEdit(data) {
+    
+  }
+
+  function warning(msg) {
+
+  }
+
+  function Req_ajax(dataIn, jump) {
+    $.ajax({
+      data: dataIn,
+      url: OL_Action_Root + "/func",
+      dataType: 'json',
+      cache: false,
+      timeout: 5000,
+      type: "post",
+      success: function (data) {
+        /*if (data.isError)
+          warning(data.data);
+        else if (jump === undefined)
+          $('.tree').innerHTML = data.data;
+        else
+          window.location.href = data.data;*/
+        if (data.isError)
+          console.log('wtf?');
+        else
+          console.log(data.data);
+        console.log('!');
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(jqXHR);
+      }
+    });
+  }
 
 });
---
+++ Project 3/server/server.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('../public'));
+
+app.get("/", function(req, res){
+  res.status(200).type('html');
+  fs.readFile('../public/html/index.html', function(err, data) {
+    if (err) {
+      console.log('Error loading home page.');
+      res.end();
+      return;
+    }
+    res.end(data);
+  })
+  console.log('Page visited.');
+});
+
+app.post('/func', urlencodedParser, function(req, res) {
+  var data = req.body;
+  console.log('Data received: ');
+  console.log(data);
+  
+  res.status(200)
+    .type('json')
+    .json({
+      isError: false,
+      data: "success!"
+    }).end();
+  
+});
+
+var server = app.listen(8888, function () {
+  console.log('Listening...');
+});
